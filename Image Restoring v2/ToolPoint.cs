5446b346e981d7db6f5254ea62b6030592e17523
elayzandra <elayzandra@yandex.ru>
2024-01-12 16:23:55 +0300

    комментарии для класса ToolPoint

--- Image Restoring v2/ToolPoint.cs
+++ Image Restoring v2/ToolPoint.cs
@@ -1,32 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Image_Restoring_v2
+﻿namespace Image_Restoring_v2
 {
-    // ToolPoint - вспомогательный класс для работы с координатами точек
+    /// <summary>
+    ///  Вспомогательный класс для работы с координатами точек.
+    ///  Строка идентификатора "T:Image_Restoring_v2.ToolPoint".
+    /// </summary>    
     public class ToolPoint
     {
-        // координаты точек
+        /// <summary>
+        /// Поле, содержащее значение абсциссы точки.
+        /// Строка идентификатора "F:Image_Restoring_v2.ToolPoint.x".
+        /// </summary>
         public double x;
+
+        /// <summary>
+        /// Поле, содержащее значение ординаты точки.
+        /// Строка идентификатора "F:Image_Restoring_v2.ToolPoint.y".
+        /// </summary>
         public double y;
 
-        //конструктор
+        /// <summary>
+        /// Конструктор.
+        /// Строка идентификатора "M:Image_Restoring_v2.ToolPoint.#ctor(System.Double,System.Double)".
+        /// </summary>
+        /// <param name="_x">Значение координаты x.</param>
+        /// <param name="_y">Значение координаты y.</param>
         public ToolPoint(double _x, double _y)
         {
             x = _x;
             y = _y;
         }
 
-        // Переписанные операторы для работы с ToolPoint
+        /// <summary>
+        /// Перегрузка оператора вычитания для класса ToolPoint.
+        /// Строка идентификатора "M:Image_Restoring_v2.ToolPoint.op_Difference(Image_Restoring_v2.ToolPoint,Image_Restoring_v2.ToolPoint)".
+        /// </summary>
+        /// <param name="_a">Вектор №1.</param>
+        /// <param name="_b">Вектор №2.</param>
+        /// <returns>Разность.</returns>  
         public static ToolPoint operator -(ToolPoint _a, ToolPoint _b)
         {
             if (_a == null)
             {
-                // Обработка, если _a равно null
-                // Возвращаем ToolPoint с нулевыми координатами
+                // Обработка, если _a равно null, возвращаем с нулевыми координатами.
                 return new ToolPoint(0, 0);
             }
 
@@ -37,21 +52,41 @@ namespace Image_Restoring_v2
 
             return new ToolPoint(_a.x - _b.x, _a.y - _b.y);
         }
+
+        /// <summary>
+        /// Перегрузка оператора сложения для класса ToolPoint.
+        /// Строка идентификатора "M:Image_Restoring_v2.ToolPoint.op_Addition(Image_Restoring_v2.ToolPoint,Image_Restoring_v2.ToolPoint)".
+        /// </summary>
+        /// <param name="_a">Вектор №1.</param>
+        /// <param name="_b">Вектор №2.</param>
+        /// <returns>Сумма.</returns>     
         public static ToolPoint operator +(ToolPoint _a, ToolPoint _b)
         {
             return new ToolPoint(_a.x + _b.x, _a.y + _b.y);
         }
+
+        /// <summary>
+        /// Перегрузка оператора умножения для класса ToolPoint.
+        /// Строка идентификатора "M:Image_Restoring_v2.ToolPoint.op_Multiplication(Image_Restoring_v2.ToolPoint,Image_Restoring_v2.ToolPoint)".
+        /// </summary>
+        /// <param name="_a">Вектор №1.</param>
+        /// <param name="s">Вектор №2.</param>
+        /// <returns>Произведение.</returns>
         public static ToolPoint operator *(ToolPoint _a, double s)
         {
             return new ToolPoint(_a.x * s, _a.y * s);
         }
 
-        // Векторное произведение
+        /// <summary>
+        /// Метод для вычисления векторного произведения двух векторов.
+        /// Строка идентификатора "M:Image_Restoring_v2.ToolPoint.CrossProduct(Image_Restoring_v2.ToolPoint,Image_Restoring_v2.ToolPoint)".
+        /// </summary>
+        /// <param name="v1">Вектор №1.</param>
+        /// <param name="v2">Вектор №2.</param>
+        /// <returns>Значение типа double, равное векторному произведению.</returns>
         public static double CrossProduct(ToolPoint v1, ToolPoint v2)
         {
             return v1.x * v2.y - v2.x * v1.y;
         }
-
     }
-
 }
